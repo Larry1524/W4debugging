b7207b8e9f9418ef6f245212796c33273a59ac33
Larry1524 <68308643+Larry1524@users.noreply.github.com>
2020-07-30 21:52:18 -0500

    Add files via upload

---
+++ larrycollectionw4.py
@@ -0,0 +1,24 @@
+# Create a collection of these authors and
+# the year they kicked the bucket;
+# print the collection in the following format:
+
+# Charles Dickens died in 1870.
+
+# Charles Dickens, 1870
+# William Thackeray, 1863
+# Anthony Trollope, 1882
+# Gerard Manley Hopkins, 1889
+
+authors = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"
+
+}
+
+print("Charles Dickens" "died in 1870")
+print("William Thackeray" "died in 1863")
+print("Anthony Trollope" "died in 1882")
+print("Gerard Manley Hopkins" "died in 1889")
+
